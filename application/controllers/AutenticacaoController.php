3a07743d7a5beb150152d62276de0bcdf8a78f3b
Kin.LHP <kin@laptop.kinlhp.com>
2016-10-18 20:36:30 -0200

    Refatoração na validação de credencial

--- application/controllers/AutenticacaoController.php
+++ application/controllers/AutenticacaoController.php
@@ -11,32 +11,15 @@ class AutenticacaoController extends Zend_Controller_Action {
     }
 
     public function entrarAction() {
-        $this->_flashMessenger = $this->_helper->getHelper('FlashMessenger');
-        $this->view->messages = $this->_flashMessenger->getMessages();
-        $formulario = new Application_Form_Autenticacao();
-        $this->view->form = $formulario;
+        $this->configurarMensagem();
+        $this->view->form = new Application_Form_Autenticacao();
         if ($this->getRequest()->isPost()) {
-            $dados = $this->getRequest()->getPost();
-            if ($formulario->isValid($dados)) {
-                $usuario = $formulario->getValue('usuario');
-                $senha = $formulario->getValue('senha');
-
-                $dbAdapter = Zend_Db_Table::getDefaultAdapter();
-
-                $authAdapter = new Zend_Auth_Adapter_DbTable($dbAdapter);
-                $authAdapter->setTableName('Credencial')
-                        ->setIdentityColumn('usuario')
-                        ->setCredentialColumn('senha');
-                $authAdapter->setIdentity($usuario)
-                        ->setCredential(hash('SHA512', $senha));
-
-                $auth = Zend_Auth::getInstance();
-
-                $result = $auth->authenticate($authAdapter);
-                if ($result->isValid()) {
-                    $info = $authAdapter->getResultRowObject(NULL, 'senha');
-                    $storage = $auth->getStorage();
-                    $storage->write($info);
+            $dadosInformados = $this->getRequest()->getPost();
+            if ($this->view->form->isValid($dadosInformados)) {
+                $adaptadorDeAutenticacao = $this->obterAdaptadorDeAutenticacao();
+                $autenticacao = $this->obterAutenticidade($adaptadorDeAutenticacao);
+                if ($autenticacao->isValid()) {
+                    $this->guardarAutenticidade($adaptadorDeAutenticacao);
                     return $this->_helper->redirector->goToRoute(array(
                         'controller' => 'index'
                     ), NULL, TRUE);
@@ -45,15 +28,40 @@ class AutenticacaoController extends Zend_Controller_Action {
                     $this->redirect('/autenticacao/entrar');
                 }
             } else {
-                $formulario->populate($dados);
+                $this->view->form->populate($dadosInformados);
             }
         }
     }
 
     public function sairAction() {
-        $auth = Zend_Auth::getInstance();
-        $auth->clearIdentity();
+        $autenticidade = Zend_Auth::getInstance();
+        $autenticidade->clearIdentity();
         return $this->_helper->redirector('index');
+    }
+
+    private function configurarMensagem() {
+        $this->_flashMessenger = $this->_helper->getHelper('FlashMessenger');
+        $this->view->messages = $this->_flashMessenger->getMessages();
+    }
+
+    private function guardarAutenticidade(Zend_Auth_Adapter_DbTable $adaptadorDeAutenticacao) {
+        $credencialSemSenha = $adaptadorDeAutenticacao->getResultRowObject(NULL, 'senha');
+        Zend_Auth::getInstance()->getStorage()
+                ->write($credencialSemSenha);
+    }
+
+    private function obterAdaptadorDeAutenticacao() {
+        $adaptadorDeAutenticacao = new Zend_Auth_Adapter_DbTable(Zend_Db_Table::getDefaultAdapter());
+        return $adaptadorDeAutenticacao->setTableName('Credencial')
+                        ->setIdentityColumn('usuario')
+                        ->setCredentialColumn('senha');
+    }
+
+    private function obterAutenticidade(Zend_Auth_Adapter_DbTable $adaptadorDeAutenticacao) {
+        $formulario = $this->view->form;
+        $adaptadorDeAutenticacao->setIdentity($formulario->getValue('usuario'))
+                ->setCredential(hash('SHA512', $formulario->getValue('senha')));
+        return Zend_Auth::getInstance()->authenticate($adaptadorDeAutenticacao);
     }
 
 }
